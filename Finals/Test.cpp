6d5d8272ed718b2806f53866235e46dd4b21c9d8
Jack <shin005004@gmail.com>
2022-03-01 12:57:53 +0900

    0301

--- Finals/Test.cpp
+++ Finals/Test.cpp
@@ -1,7 +1,24 @@
 #include <cstdio>
+#include <algorithm>
+using namespace std;
+
+struct C{
+    int to;
+    int cost;
+};
 
 int main(void)
 {
+    C c = C{1, 2};
+    printf("%d %d\n", c.to, c.cost);
+    
     int x = 1;
     printf("%c \n", x + 'A');
+
+    int a[] = {1,3, 2, 5, 4};
+    sort(a, a + 5);
+    for(int x: a)
+        printf("%d ", x);
+
+       
 }
